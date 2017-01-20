93daec3b7728ce102663b9572d043ebe67b921ef
Valentin Silvestre <vasilvestre@laposte.net>
2017-01-20 15:53:12 +0100

    clean routes

--- src/routes.php
+++ src/routes.php
@@ -1,40 +1,44 @@
 <?php
 // Routes
-
-$app->post('/techniciens', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
 //    $firebase = Firebase::fromServiceAccount(__DIR__.'/Firebase/ppe4-db-firebase-adminsdk-094r8-34a6a11e6d.json');
 //    $database = $firebase->getDatabase();
 //    $reference = $database->getReference('Techniciens/0');
 //    $snapshot = $reference->getSnapshot();
-    $input = json_decode($request->getBody());
-    $position = R::dispense('position');
-
-    return $newResponse;
-});
 
 $app->get('/latlong', function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
     $this->db;
-    $positions = R::getAll('SELECT * FROM position');
+    $positions = R::findAll('position');
     foreach ($positions as &$position){
-        $counter = R::findOne('position',
-            'IMEI = ?',[$position['IMEI']]
-        );
-        $counter->counter -= 1;
+        $position->counter -= 1;
     }
     R::storeAll($positions);
-    $positions = R::inspect('position');
     $newResponse = $response->withHeader('Content-type', 'application/json');
     return $newResponse->withJson($positions);
 });
 
-
-
-$app->get('/[{name}]', function ($request, \Slim\Http\Response $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.html', $args);
+$app->post('/latlong',function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
+    $this->db;
+    $params = $request->getHeaders();
+    if ($params['HTTP_IMEI'] == null or $params['HTTP_X'] == null or $params['HTTP_Y'] == null){
+        return $response->write('Veuillez spécifier votre IMEI et vos positions');
+    }
+    $presence = R::findOne('position', 'imei = ?', [$params['HTTP_IMEI'][0]]);
+    if ($presence == null) {
+        $position = R::dispense('position');
+        $position->imei = $params['HTTP_IMEI'][0];
+        $position->x = $params['HTTP_X'][0];
+        $position->y = $params['HTTP_Y'][0];
+        $position->updated_at = time();
+        $position->created_at = time();
+        $position->counter = 10;
+        R::store($position);
+        $message = 'Technicien crée avec succes';
+    }else{
+        $presence->x = $params['HTTP_X'][0];
+        $presence->y = $params['HTTP_Y'][0];
+        $presence->updated_at = time();
+        R::store($presence);
+        $message = 'Technicien mit à jour !';
+    }
+    return $response->write($message);
 });
-
-
