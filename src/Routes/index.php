4d2631c5369d9ee76ea4e7cb15c70bb02f0bb287
Valentin Silvestre <vasilvestre@laposte.net>
2017-02-26 00:05:33 +0100

    wip

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,5 +1,15 @@
 <?php
 
-$app->get('/', function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
+$app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/login_check', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+    $this->db;
+    $params = $request->getHeaders();
+    $result = R::findOne('technicien', 'login = :login and password = :password', [
+        ':login' => $params['HTTP_LOGIN'][0],
+        ':password' => $params['HTTP_PASSWORD'][0]
+    ]);
+    return $result == null ? 'nop' : 'true';
 });
