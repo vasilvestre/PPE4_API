990f4ca1bf4224c200e098cc0643ef594b5f78ce
Valentin Silvestre <vasilvestre@laposte.net>
2017-05-17 15:43:44 +0200

    link new entreprise to old intervention

--- src/Routes/script.php
+++ src/Routes/script.php
@@ -3,11 +3,12 @@
 $app->post('/boucle_intervention', function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
     $this->db;
     $interventions = R::findAll('intervention');
-    foreach ($interventions as $intervention){
+    foreach ($interventions as &$intervention){
         $entreprise = R::dispense('entreprise');
         $entreprise->nom = $intervention->entreprise;
         $entreprise->address = $intervention->address;
         R::store($entreprise);
+        $intervention->id_entreprise = $entreprise->id;
     }
     $interventions = R::findAll('entreprise');
     $newResponse = $response->withHeader('Content-type', 'application/json');
